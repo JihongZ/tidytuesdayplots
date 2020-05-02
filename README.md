c0d6531b4289ee645d90b662da92a93f315ad7d1
JihongZ <jihong-zhang@uiowa.edu>
2020-05-02 10:50:28 -0500

    new week

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # tidytuesdayplots
 My #tidytuesdays Plots
+
+Week 18: 2020-04-28 Broadway Musicals
+![](code/Line_Plot.png)
---
+++ code/data_analysis.R
@@ -0,0 +1,30 @@
+library(tidyverse)
+library(ggplot2)
+
+dt <- read.csv("../data/df_show_gross.csv")
+
+rankbyseats <- 
+  dt %>% 
+  group_by(show) %>% 
+  summarize(total_seats = max(seats_sold)) %>% 
+  arrange(desc(total_seats)) %>% 
+  mutate(rank = 1:nrow(.))
+
+dt <- dt %>% 
+  left_join(rankbyseats, by="show")
+
+top10 <- filter(dt, rank < 11)
+top10$year <- format(as.Date(top10$week_ending,format="%Y-%m-%d"), "%Y")
+
+top10 <- top10 %>% 
+  group_by(show, year) %>% 
+  summarise(n_total = sum(total_seats))
+## Plot the top ten shows
+top10 %>% 
+  ggplot(aes(x=year, y=n_total, color=show, group=show)) +
+  geom_line(size=1.5)+
+  theme_minimal()+
+  theme(legend.position = "bottom")+
+  labs(y="Total Seat Sold at that year")+
+  scale_x_discrete(breaks = seq(1985, 2020, 10))
+ggsave("../code/Line_Plot.png")
